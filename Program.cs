c30a0958c20c57a6ec74c3ae1efd2f3c629fc5bb
DermisZabala <dermiszabala@gmail.com>
2024-07-17 20:35:21 -0400

    Conexion con archivo y creacion de destructor

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+
+namespace Destructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ManejoArchivo miDocumento = new();
+
+            Console.WriteLine();
+            miDocumento.Mensaje();
+        }
+    }
+
+    class ManejoArchivo
+    {
+        StreamReader archivo = null;
+
+        int contador;
+
+        string lineas;
+
+        public ManejoArchivo()
+        {
+            contador = 0;
+
+            //conexión con el archivo.txt
+            archivo = new StreamReader(@"C:\Users\HP PROBOOK\OneDrive\Desktop\Manejo de archivo.txt");
+
+            while((lineas = archivo.ReadLine()) != null)
+            {
+                Console.WriteLine(lineas);
+                contador++;
+            }
+        }
+
+        public void Mensaje()
+        {
+            Console.WriteLine("Este archivo tiene {0} líneas", contador);
+        }
+
+
+        //creacion del destructor
+        ~ManejoArchivo()
+        {
+            //cerrando conexión con el archivo
+            archivo.Close();
+        }
+    }
+}
